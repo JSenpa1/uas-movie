ddb187b38cb5dbe4452368c29238a84e909525fb
JSenpa1 <41582843+JSenpa1@users.noreply.github.com>
2023-05-22 15:27:07 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getMovieList, searchMovie } from "./api";
 
-function App() {
+const App = () => {
+
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect( () =>  {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+          <div className="Movie-wrapper" key={i}>
+            <div className="Movie-title">{movie.title}</div>
+            <img 
+              className="Movie-image" 
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
+            />
+            <div className="Movie-date">Release: {movie.release_date}</div>
+            <div className="Movie-rate">{movie.vote_average}</div>
+          </div>
+      );
+    })
+  }
+
+  const search = async (q) => {
+    const query = await searchMovie(q)
+    setPopularMovies(query.results)
+  }
+
+  console.log({ popularMovies: popularMovies });
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="App-header">
+
+        <h1>Hello World</h1>
+        <input 
+          placeholder="cari film kesayangan..." 
+          className="Movie-search" 
+          onChange={({ target }) => search(target.value)}
+        />
+
+
+        <div className="Movie-container">
+            <PopularMovieList />
+        </div>
+      </div>
     </div>
   );
 }
