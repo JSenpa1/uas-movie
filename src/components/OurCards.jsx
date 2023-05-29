eca0a2319f9420efe55abffe9771b3c851b448e8
KamuNanyaAkuSiapa <117794925+KamuNanyaAkuSiapa@users.noreply.github.com>
2023-05-29 15:16:54 +0700

    tork first commit

--- src/components/OurCards.jsx
+++ src/components/OurCards.jsx
@@ -3,13 +3,13 @@ import './OurCards.css'
 
 function OurCards(props) {
   return (
-    <div className='card-bungkus'>
-        <div className='card'>
-            <img src={props.image} alt='pfp'/>
-            <h1 className='text-white'>{props.name}</h1>
-            <h2>{props.aboutMe}</h2>
-        </div>
-    </div>
+      <div className='card-bungkus'>
+          <div className='card'>
+              <img src={props.image} alt='pfp'/>
+              <h1 className='text-white'>{props.name}</h1>
+              <h2>{props.aboutMe}</h2>
+          </div>
+      </div>
   );
 }
 
